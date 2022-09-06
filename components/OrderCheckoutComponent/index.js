ad225ec91ca0939fa4f5ab98879edd433a8de78b
vusales <vusale.safarova95@mail.ru>
2022-09-06 17:03:44 +0400

    checkout order

---
+++ components/OrderCheckoutComponent/index.js
@@ -0,0 +1,67 @@
+import React from "react"
+import styles from "./index.module.scss";
+import ListIcon from '@mui/icons-material/List';
+import Link from "next/link";
+import { DesktopTablet , Desktop} from "../../ui/Breakpoints";
+import { Grid } from "@mui/material";
+import AddLinkIcon from '@mui/icons-material/AddLink';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+
+const OrderCheckoutComponent = () => {
+    return (
+        <Grid item xs={12} >
+            <Grid container spacing={2} p={2}  >
+                <Grid item  xs={12} p={2} pl={0} className={styles.titleBorder}>
+                    <div className={styles.cover}>
+                        <p className={styles.title} >Checkout</p>
+                    </div>
+                </Grid>
+                <Grid item  xs={12}>
+                <div className={styles.cover}>
+                    <form>
+                        <div className={styles.inputContainer}>
+                            <div className={styles.iconContainer}>
+                                <AddLinkIcon />
+                            </div>
+
+                            <input  
+                            type="url"
+                            placeholder="Insert link"
+                            />
+
+                        </div>
+
+                        <div className={styles.dividerCon}>
+                            <div className={styles.inputContainer}>
+                                <div className={styles.iconContainer}>
+                                    <AlternateEmailIcon />
+                                </div>
+
+                                <input  
+                                type="email"
+                                placeholder="Your email"
+                                />
+
+                            </div>
+                            <div className={styles.inputContainer}>
+                                <div className={styles.iconContainer}>
+                                    <PhoneIphoneIcon />
+                                </div>
+
+                                <input  
+                                type="tel"
+                                placeholder="Your phone number"
+                                />
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default OrderCheckoutComponent ; 
--- pages/order/index.js
+++ pages/order/index.js
@@ -3,6 +3,7 @@ import styles from "./index.module.scss";
 import { Grid , Container , Paper } from "@mui/material";
 import CommonLayout from "../../layout/commonLayout";
 import OrderListComponent from "../../components/OrderListComponent";
+import OrderCheckoutComponent from "../../components/OrderCheckoutComponent";
 
 const Order = () => {
     return (
@@ -11,8 +12,7 @@ const Order = () => {
                 <Paper elevation={2} className={styles.paper }>
                     <Grid container spacing={2} >
                         <OrderListComponent />
-
-
+                        <OrderCheckoutComponent />
                     </Grid>
                 </Paper>
             </Container>
