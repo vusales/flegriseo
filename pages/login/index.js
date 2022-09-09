c720ca7fb24d37c7cd19e1b705e93dacfdd3b933
vusales <vusale.safarova95@mail.ru>
2022-09-09 18:31:28 +0400

    responsive

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,19 +2,77 @@ import React from "react";
 import styles from "./index.module.scss";
 import { Grid , Container , Paper } from "@mui/material";
 import CommonLayout from "../../layout/commonLayout";
-import OrderListComponent from "../../components/OrderListComponent";
-import OrderCheckoutComponent from "../../components/OrderCheckoutComponent";
-import OrderSubscriptions from "../../components/OrderSubscriptions";
-import SelectPaymantMethod from "../../components/SelectPaymantMethod";
-import OrderCheckout from "../../components/OrderCheckout";
+import StayCurrentPortraitIcon from '@mui/icons-material/StayCurrentPortrait';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import InputMask from 'react-input-mask';
+import Link from "next/link";
 
 
 const Login = () => {
     return(
         <CommonLayout>
             <Container>
-            Login
+               <Paper elevation={2} className={styles.papper}>
+               <form className={styles.inputBaseContainer}>
+                    <Grid container spacing={2} p={3}>
+                        <Grid item xs={12}>
+                            <div className={styles.titleCon} >
+                                <h1>Login</h1>
+                                <div className={styles.line}></div>
+                            </div>
+                        </Grid>
 
+                        <Grid item xs={12}>
+                            <div className={styles.loginContainer}>
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <StayCurrentPortraitIcon />
+                                        </div>
+
+                                        <InputMask mask="(99)-999-99-99" 
+                                        onChange={(e)=>{}}
+                                        >
+                                            {() => (
+                                            <input
+                                                placeholder="+994 |"
+                                                id="number"
+                                            />
+                                            )}
+                                        </InputMask>
+                                    </div>
+
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <LockOpenIcon />
+                                        </div>
+
+                                        <input  
+                                        type="text"
+                                        placeholder="Password"
+                                        />
+                                    </div>
+                            </div>
+                        </Grid>  
+
+                        <Grid item xs={12}>
+                            <div className={styles.links}>
+                                <Link href="/">
+                                    <a  className={styles.bannerButton}>Forgot Password?</a>
+                                </Link>
+                                <Link href="/signUp">
+                                    <a  className={styles.bannerButton}>Registration</a>
+                                </Link>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <div className={styles.buttons}>
+                                <button>Sign in</button>
+                            </div>
+                        </Grid>
+
+                    </Grid>
+                </form>
+               </Paper>
             </Container>
         </CommonLayout>
     )
--- pages/signUp/index.js
+++ pages/signUp/index.js
@@ -2,19 +2,121 @@ import React from "react";
 import styles from "./index.module.scss";
 import { Grid , Container , Paper } from "@mui/material";
 import CommonLayout from "../../layout/commonLayout";
-import OrderListComponent from "../../components/OrderListComponent";
-import OrderCheckoutComponent from "../../components/OrderCheckoutComponent";
-import OrderSubscriptions from "../../components/OrderSubscriptions";
-import SelectPaymantMethod from "../../components/SelectPaymantMethod";
-import OrderCheckout from "../../components/OrderCheckout";
+import StayCurrentPortraitIcon from '@mui/icons-material/StayCurrentPortrait';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import PersonIcon from '@mui/icons-material/Person';
+import InputMask from 'react-input-mask';
+import Link from "next/link";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 
 const SignUp = () => {
     return(
         <CommonLayout>
             <Container>
-            SignUp
+               <Paper elevation={2} className={styles.papper}>
+               <form className={styles.inputBaseContainer}>
+                    <Grid container spacing={2} p={3}>
+                        <Grid item xs={12}>
+                            <div className={styles.titleCon} >
+                                <h1>Registration</h1>
+                                <div className={styles.line}></div>
+                            </div>
+                        </Grid>
 
+                        <Grid item xs={12}>
+                            <div className={styles.loginContainer}>
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <PersonIcon />
+                                        </div>
+
+                                        <input  
+                                        type="text"
+                                        placeholder="name"
+                                        />
+                                    </div>
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <PersonIcon />
+                                        </div>
+
+                                        <input  
+                                        type="text"
+                                        placeholder="surname"
+                                        />
+                                    </div>
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <AlternateEmailIcon />
+                                        </div>
+
+                                        <input  
+                                        type="email"
+                                        placeholder="email"
+                                        />
+                                    </div>
+
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <StayCurrentPortraitIcon />
+                                        </div>
+
+                                        <InputMask mask="(99)-999-99-99" 
+                                        onChange={(e)=>{}}
+                                        >
+                                            {() => (
+                                            <input
+                                                placeholder="phone"
+                                                id="number"
+                                            />
+                                            )}
+                                        </InputMask>
+                                    </div>
+
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <LockOpenIcon />
+                                        </div>
+
+                                        <input  
+                                        type="text"
+                                        placeholder="password"
+                                        />
+                                    </div>
+
+                                    
+                                    <div className={styles.inputContainer}>
+                                        <div className={styles.iconContainer}>
+                                            <LockOpenIcon />
+                                        </div>
+
+                                        <input  
+                                        type="text"
+                                        placeholder="password again"
+                                        />
+                                    </div>
+                            </div>
+                        </Grid>  
+
+                        <Grid item xs={12}>
+                            <div className={styles.links}>
+                                {/* <Link href="/">
+                                    <a  className={styles.bannerButton}>Forgot Password?</a>
+                                </Link> */}
+                                <Link href="/login">
+                                    <a  className={styles.bannerButton}>Login</a>
+                                </Link>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <div className={styles.buttons}>
+                                <button>Sign up</button>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </form>
+               </Paper>
             </Container>
         </CommonLayout>
     )
