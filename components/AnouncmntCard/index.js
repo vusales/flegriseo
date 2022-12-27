4f9027dec5326162bcf3a479c4686af0ca5aee57
vusales <vusale.safarova95@mail.ru>
2022-12-27 11:14:19 +0400

    fortawesome setting

--- components/AnouncmntCard/index.js
+++ components/AnouncmntCard/index.js
@@ -1,12 +1,20 @@
 import React from "react"; 
 import styles from "./index.module.scss";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  solid,
+  regular,
+  brands,
+  icon
+} from '@fortawesome/fontawesome-svg-core/import.macro';
+
 
 const AnouncmentCard = ({data}) =>{
     const {icon , title , description } =data ;
     return (
         <div className={styles.cardContiner}>
-            {icon}
+            <FontAwesomeIcon icon={solid('coffee')} size="lg" />
             <p className={styles.title}>{title}</p>
             <p>{description}</p>
         </div>
